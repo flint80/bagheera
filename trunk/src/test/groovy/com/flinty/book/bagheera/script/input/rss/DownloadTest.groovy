2393e60ca1f372e3757eae6e6672feb4b0b78511
flinty80@gmail.com <flinty80@gmail.com@8c4bb94c-6b76-892c-3fb0-abd892289548>
2013-03-03 16:09:09 +0000

    migrating to http-client v4

--- trunk/src/test/groovy/com/flinty/book/bagheera/script/input/rss/DownloadTest.groovy
+++ trunk/src/test/groovy/com/flinty/book/bagheera/script/input/rss/DownloadTest.groovy
@@ -2,10 +2,17 @@
 
 package com.flinty.book.bagheera.script.input.rss
 
-import org.apache.commons.httpclient.Header
-import org.apache.commons.httpclient.HttpClient
-import org.apache.commons.httpclient.methods.GetMethod
-import org.apache.commons.httpclient.methods.PostMethod
+import org.apache.http.Header
+import org.apache.http.HttpEntity
+import org.apache.http.HttpResponse
+import org.apache.http.NameValuePair
+import org.apache.http.client.HttpClient
+import org.apache.http.client.entity.UrlEncodedFormEntity
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.impl.client.DefaultHttpClient
+import org.apache.http.message.BasicNameValuePair
+import org.apache.http.util.EntityUtils
+
 
 
 
@@ -14,23 +21,27 @@ import org.apache.commons.httpclient.methods.PostMethod
 class DownloadTest extends GroovyTestCase{
 
 	void testRss(){
-		HttpClient client = new HttpClient()
+		HttpClient client = new DefaultHttpClient()()
 		// establish a connection within 5 seconds
-		client.getHttpConnectionManager().getParams()
-				.setConnectionTimeout(5000)
-		PostMethod postMethod = new PostMethod("http://www.forumhouse.ru/login/login")
-		postMethod.setParameter("login", 'nataliana')
-		postMethod.setParameter("password", 'chuchito')
-		client.executeMethod(postMethod)
-		GetMethod method = new GetMethod('http://www.forumhouse.ru/attachments/665634/')
-		method.setFollowRedirects(true)
-		// execute the method
-		client.executeMethod(method)
-		byte[] content = method.getResponseBody()
-		for(Header header : method.getResponseHeaders()){
-			println "${header.getName()}: ${header.getValue()}"
+		client.getParams().setIntParameter("http.connection.timeout", 5000)
+		HttpPost httpPost = new HttpPost("http://www.forumhouse.ru/login/login");
+		List <NameValuePair> nvps = new ArrayList <NameValuePair>();
+		nvps.add(new BasicNameValuePair("login", "test"));
+		nvps.add(new BasicNameValuePair("password", "test"));
+		httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+		HttpResponse response = client.execute(httpPost);
+
+		try {
+			for(Header header : response.getAllHeaders()){
+				println "${header.getName()}: ${header.getValue()}"
+			}
+			HttpEntity entity = response.getEntity();
+			byte content = EntityUtils.toByteArray(entity)
+			println "content length is ${content.length}"
+			new File('test/image.png').setBytes(content)
+			EntityUtils.consume(entity);
+		} finally {
+			httpPost.releaseConnection();
 		}
-		println "content length is ${content.length}"
-		new File('test/image.png').setBytes(content)
 	}
 }
